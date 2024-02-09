c706709760536f216325df0b3262cd5aac644c9e
Dominick Novello <dominicknovello64@gmail.com>
2024-02-09 16:33:44 -0500

    widen images

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
+import { useState, useEffect } from "react";
 import './App.css';
+import SearchIcon from './search.svg';
+import GameCard from "./GameCard";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+
+
+const App = () => {
+
+  
+
+    const [games, setGames] = useState([]);
+    const [gameName, setGameName] = useState();
+
+
+
+    const searchGames = async (gameName) => {
+        const response = await fetch(`https://api.rawg.io/api/games?key=3a6b92cff7bc48a7a66a3f5c492de738&search=${gameName}`)
+        const data = await response.json();
+        const gamelist = data.results;
+        setGames(gamelist);
+        
+        
+    }
+    
+
+    useEffect(() => {
+       searchGames("")
+   }, []);
+
+  
+
+    return (
+
+        <div>
+            <h1>GameStop</h1>
+
+            <div className="search">
+                <input
+                    placeholder="Search for Games"
+                    value={gameName}
+                    onChange={(e) => {setGameName(e.target.value)  }}
+                />
+                <img
+                   src={SearchIcon}
+                    alt="search"
+                    onClick={() => {searchGames(gameName) }}
+                />
+            </div>
+    
+            {
+                games?.length > 0
+                    ? (
+                        <div className="container">
+                        {games.map((game) => <GameCard game={game} />)}
+                        </div>
+                    ) :
+                    (
+                       <div className="empty">
+                            <h2>No games found</h2>
+
+                        </div>
+
+                    )
+
+                    }
+
+
+
+
+        </div >
+
+
+        );
+                  };
+                  
 
 export default App;
+
+
---
+++ src/GameCard.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+
+const GameCard = ({ game }) => {
+    return (
+        <div className="movie">
+            <div>
+                <p>{game.released}</p>
+            </div>
+            <div>
+                <img
+                    src={game.background_image || "https://via.placeholder.com/400"}
+                    alt={game.name}
+                />
+            </div>
+            <div>
+                <span>{game.platforms.map(platform => platform.platform.name).join(', ')}</span>
+                <h3>{game.name}</h3>
+            </div>
+        </div>
+    );
+};
+
+export default GameCard;
